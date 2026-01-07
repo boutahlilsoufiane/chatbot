6c8006244971703702c482634021f9f318a06259
soufianeboutahlil <soufiane.boutahlil@outlook.com>
2026-01-07 09:45:38 +0100

    feat: add chat markup

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,65 +1,60 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+
+      <div className="flex justify-center  items-center mt-5">
+        <div id="chat-content" className="w-full max-w-md bg-white rounded-xl shadow-lg">
+
+          <div className="divide-y divide-white">
+            <div className="flex p-5">
+
+              <div className="flex flex-col ml-3 my-auto">
+                <div className="font-medium text-md">Support</div>
+              </div>
+            </div>
+
+            <div className="bg-[#F7F7F7] p-5 space-y-3 shadow-inner">
+              <div className="flex justify-end">
+                <div className="bg-[#333333] px-3 py-2 text-white rounded-2xl rounded-br-none shadow-md">Hello, Roungridh Him!</div>
+              </div>
+
+              <div className="flex flex-col justify-start gap-3">
+                <div className="flex">
+                  <div className="bg-white px-3 py-2 text-gray-700 rounded-2xl rounded-bl-none shadow-md">Good morining, Anderson!</div>
+                </div>
+                <div className="flex">
+                  <div className="bg-white px-3 py-2 text-gray-700 rounded-2xl rounded-bl-none shadow-md">How are you doing?</div>
+                </div>
+              </div>
+
+              <div className="flex justify-end">
+                <div className="bg-[#333333] px-3 py-2 text-white rounded-2xl rounded-br-none shadow-md">I'm fine... how about you?</div>
+              </div>
+
+              <div className="flex justify-start">
+                <div className="bg-white px-3 py-2 text-gray-700 rounded-2xl rounded-bl-none shadow-md">Great 😄</div>
+              </div>
+
+              <div className="flex justify-end">
+                <div className="bg-[#333333] px-3 py-2 text-white rounded-2xl rounded-br-none shadow-md">This is a chat sample! You can use it <br/> with your project.</div>
+              </div>
+
+              <div className="flex justify-start">
+                <div className="bg-white px-3 py-2 text-gray-700 rounded-2xl rounded-bl-none shadow-md">I really appreciated... ❤️</div>
+              </div>
+            </div>
+
+            <div className="p-5">
+              <div className="flex w-full">
+                <input className="w-full outline-none border border-gray-200 px-3 py-1.5 rounded-l-md" placeholder="Type message here..."/>
+                  <button type="button" className="bg-[#333333] px-2.5 rounded-r-md">
+                    <svg className="w-6 h-6"fill="#FFFFFF" version="1.1" id="Layer_1" viewBox="796 707.122 200 200" enable-background="new 796 707.122 200 200" >
+                      <path d="M798.671,800.534c-1.559,0.651-2.6,2.148-2.667,3.837s0.849,3.264,2.351,4.039l49.397,25.494l10.707,58.754  c0.312,1.707,1.608,3.066,3.3,3.457s3.453-0.262,4.481-1.66l27.193-36.976l65.524,33.817c1.226,0.633,2.679,0.646,3.916,0.037  c1.237-0.61,2.112-1.771,2.358-3.128L996,718.017L798.671,800.534z M869.045,844.893l-21.294-10.99l112.881-81.413L869.045,844.893z  " />
+                    </svg>
+                  </button>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+      </div>
+  )
 }
