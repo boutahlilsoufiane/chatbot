e7a4b5a73aa055a191156ac9426f5a40e3f84865
soufianeboutahlil <soufiane.boutahlil@outlook.com>
2026-01-07 10:21:01 +0100

    feat: add post question api

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -25,6 +25,13 @@ export default function Home() {
         setMessage(null)
       })
   }, [])
+  
+
+  const onSend = () => {
+    axios.post("/get-answer", {
+      question
+    })
+  }  
 
   return (
 
@@ -56,7 +63,7 @@ export default function Home() {
           <div className="p-5">
             <div className="flex w-full">
               <input value={question} onChange={(e) => setQuestion(e.target.value)} className="w-full outline-none border border-gray-200 px-3 py-1.5 rounded-l-md" placeholder="Type message here..." />
-              <button type="button" className="bg-[#333333] px-2.5 rounded-r-md">
+              <button onClick={onSend} type="button" className="bg-[#333333] px-2.5 rounded-r-md">
                 <svg className="w-6 h-6" fill="#FFFFFF" version="1.1" id="Layer_1" viewBox="796 707.122 200 200" enable-background="new 796 707.122 200 200" >
                   <path d="M798.671,800.534c-1.559,0.651-2.6,2.148-2.667,3.837s0.849,3.264,2.351,4.039l49.397,25.494l10.707,58.754  c0.312,1.707,1.608,3.066,3.3,3.457s3.453-0.262,4.481-1.66l27.193-36.976l65.524,33.817c1.226,0.633,2.679,0.646,3.916,0.037  c1.237-0.61,2.112-1.771,2.358-3.128L996,718.017L798.671,800.534z M869.045,844.893l-21.294-10.99l112.881-81.413L869.045,844.893z  " />
                 </svg>
