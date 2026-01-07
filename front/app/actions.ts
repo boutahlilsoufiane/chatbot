89bc3ae07fb6c247a6e2674c8f40c56fa1293272
soufianeboutahlil <soufiane.boutahlil@outlook.com>
2026-01-07 11:02:56 +0100

    fix: add missing types

--- front/app/actions.ts
+++ front/app/actions.ts
@@ -1,6 +1,6 @@
 import axios from "./axios"
 
-export async function getAnswer(prevState, queryData) {
+export async function getAnswer(prevState: unknown, queryData: FormData) {
     await axios.post("/get-answer", {
         question : queryData.get("question")
     })
