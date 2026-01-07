cf050bb93e7d84903673943ddcdd713df1c2efe0
soufianeboutahlil <soufiane.boutahlil@outlook.com>
2026-01-07 09:21:36 +0100

    fix: fix ts types

--- server/server.ts
+++ server/server.ts
@@ -1,18 +1,29 @@
 import express from "express";
 import { connectDB, getNativeDB } from "./db.ts";
 
-const { Request, Response } = express;
 
 const app = express();
 const PORT = 7000;
+
+interface Answer {
+  keywords: string[];
+  response: string;
+}
+
+interface History {
+  message: string;
+  sender: string;
+  date: Date;
+}
+
+
 
 (async () => {
   await connectDB();
 
   app.use(express.json());
 
-  app.get("/api/history", async (req: Request, res: Response) => {
-    console.log("get request")
+  app.get("/api/history", async (req: express.Request, res: express.Response) => {
     try {
       const db = getNativeDB();
       const rows = await db.collection("history").find().toArray();
@@ -22,12 +33,12 @@ const PORT = 7000;
     }
   });
 
-  app.post("/api/get-answer", async (req: Request, res: Response) => {
+  app.post("/api/get-answer", async (req: express.Request, res: express.Response) => {
     try {
 
       const { question } = req.body;
       const db = getNativeDB();
-      const rows = await db.collection("predefined_answers").find().toArray();
+      const rows = await db.collection<Answer>("predefined_answers").find().toArray();
 
       const lowerMessage = question.toLowerCase();
       let botResponse = "Could you rephrase your question?";
@@ -40,16 +51,18 @@ const PORT = 7000;
       }
 
       const questionObject = {
-        message : question,
-        sender : "customer"
+        message: question,
+        sender: "user",
+        date: new Date()
       }
 
       const answerObject = {
-        message : botResponse,
-        sender : "bot"
+        message: botResponse,
+        sender: "bot",
+        date: new Date()
       }
 
-      await db.collection("history").insertMany([questionObject, answerObject]);
+      await db.collection<History>("history").insertMany([questionObject, answerObject]);
 
       res.json(botResponse);
     } catch (err) {
